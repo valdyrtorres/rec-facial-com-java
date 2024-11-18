b9342de49beccaf4a8dd5530e4b55113417aeba8
Valdir Dell Latitude <valdyrtorres@yahoo.com.br>
2024-11-18 18:19:20 -0300

    detectanto a face

--- src/reconhecimento/Captura.java
+++ src/reconhecimento/Captura.java
@@ -7,6 +7,15 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
 
 public class Captura {
     public static void main(String[] args) throws FrameGrabber.Exception {
@@ -15,12 +24,27 @@ public class Captura {
         OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0);
         camera.start();
 
+        CascadeClassifier detectorFace = new CascadeClassifier("src\\recursos\\haarcascade_frontalface_alt.xml");
+
         CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
         Frame frameCapturado = null;
+        Mat imagemColorida = null;
+        // Fazer uma correção, pois o programa fica preso mesmo fechando o frame
         while ((frameCapturado = camera.grab()) != null) {
+            imagemColorida = converteMat.convert(frameCapturado);
+            Mat imagemCinza = new Mat();
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+            RectVector facesDetectadas = new RectVector();
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150), new Size(500, 500));
+            for(int i=0; i < facesDetectadas.size(); i++) {
+                Rect dadosFace = facesDetectadas.get(0);
+                rectangle(imagemColorida, dadosFace, new Scalar(0, 0, 255, 0));
+            }
             if (cFrame.isVisible()) {
                 cFrame.showImage(frameCapturado);
             }
         }
+        cFrame.dispose();
+        camera.stop();
     }
 }
